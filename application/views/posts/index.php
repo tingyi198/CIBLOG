f99661b8c8ee1484e681a751b6206acc37eaee10
chloelee <chloelee@omusic.com.tw>
2019-12-18 18:22:25 +0800

    update

---
+++ application/views/posts/index.php
@@ -0,0 +1,10 @@
+<h2><?= $title ?></h2>
+
+<?php foreach ($posts as $post) : ?>
+	<h3> <?= $post['title'] ?> </h3>
+	<small class="post-date">Post on: <?= $post['created_at'] ?> </small>
+	<p><?= $post['body'] ?></p>
+
+	<p><a class="btn btn-default" href="<?= site_url('/posts/' . $post['slug']); ?>">Read More...</a></p>
+
+<?php endforeach; ?>
---
+++ application/views/templates/header.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+
+</head>
+<title>CI_Blog</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/journal/bootstrap.min.css">
+<link rel="stylesheet" href="<?= base_url() ?>/assets/css/style.css">
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="<?= base_url() ?>">CiBlog</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link active" href="<?php echo base_url(); ?>">Home <span class="sr-only"></span></a>
+				<a class="nav-item nav-link" href="<?php echo base_url(); ?>about">About</a>
+				<a class="nav-item nav-link" href="<?php echo base_url(); ?>posts">Blogs</a>
+
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
