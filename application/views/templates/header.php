1d2c1fe02e55f7c58425c9021d016d652b115a7c
chloelee <chloelee@omusic.com.tw>
2019-12-19 09:39:17 +0800

    改header style

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,12 +4,12 @@
 
 </head>
 <title>CI_Blog</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/journal/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/minty/bootstrap.min.css">
 <link rel="stylesheet" href="<?= base_url() ?>/assets/css/style.css">
 
 <body>
 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<a class="navbar-brand" href="<?= base_url() ?>">CiBlog</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
