f72d69085f05e2dd43a319e142da9fa1622eb7b0
chloelee <chloelee@omusic.com.tw>
2019-12-23 16:39:48 +0800

    check if email and username exist

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -7,8 +7,8 @@ class Users extends CI_Controller
 		$data['title'] = 'Sign up';
 
 		$this->form_validation->set_rules('name', 'Name', 'required');
-		$this->form_validation->set_rules('username', 'Username', 'required');
-		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+		$this->form_validation->set_rules('username', 'Username', 'required|callback_check_username_exists');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|callback_check_email_exists');
 		$this->form_validation->set_rules('password', 'Password', 'required');
 		$this->form_validation->set_rules('confirmPassword', 'Confirm Password', 'required|matches[password]');
 
@@ -28,5 +28,21 @@ class Users extends CI_Controller
 
 			redirect('posts');
 		}
+	}
+
+	// Check if username exists
+	function check_username_exists($username)
+	{
+
+		$this->form_validation->set_message('check_username_exists', 'The username is taken. Please choose a different one.');
+
+		return $this->User_model->check_username_exists($username);
+	}
+
+	// Check if email exists
+	function check_email_exists($email)
+	{
+		$this->form_validation->set_message('check_email_exists', 'The email is taken. Please choose a different one.');
+		return $this->User_model->check_email_exists($email);
 	}
 }
