64f4fdc5a450265267b581bac99115c1aadedc2e
chloelee <chloelee@omusic.com.tw>
2019-12-23 17:52:09 +0800

    modify register form style

--- application/views/users/register.php
+++ application/views/users/register.php
@@ -1,39 +1,42 @@
-<h2><?= $title; ?></h2>
-
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('users/register'); ?>
 
-<div class="form-group">
-	<label>Name</label>
-	<input type="text" class="form-control" name="name" placeholder="Name">
-</div>
+<div class="row">
+	<div class="col-md-4 offset-md-4">
+		<h1 class="text-center"><?= $title; ?></h1>
+		<div class="form-group">
+			<label>Name</label>
+			<input type="text" class="form-control" name="name" placeholder="Name">
+		</div>
 
-<div class="form-group">
-	<label>Zipcode</label>
-	<input type="text" class="form-control" name="zipcode" placeholder="Zipcode">
-</div>
+		<div class="form-group">
+			<label>Zipcode</label>
+			<input type="text" class="form-control" name="zipcode" placeholder="Zipcode">
+		</div>
 
-<div class="form-group">
-	<label>Email</label>
-	<input type="email" class="form-control" name="email" placeholder="Email">
-</div>
+		<div class="form-group">
+			<label>Email</label>
+			<input type="email" class="form-control" name="email" placeholder="Email">
+		</div>
 
-<div class="form-group">
-	<label>Username</label>
-	<input type="text" class="form-control" name="username" placeholder="Username">
-</div>
+		<div class="form-group">
+			<label>Username</label>
+			<input type="text" class="form-control" name="username" placeholder="Username">
+		</div>
 
-<div class="form-group">
-	<label>Password</label>
-	<input type="password" class="form-control" name="password" placeholder="Password">
-</div>
+		<div class="form-group">
+			<label>Password</label>
+			<input type="password" class="form-control" name="password" placeholder="Password">
+		</div>
 
-<div class="form-group">
-	<label>Confirm Password</label>
-	<input type="password" class="form-control" name="confirmPassword" placeholder="Confirm Password">
-</div>
+		<div class="form-group">
+			<label>Confirm Password</label>
+			<input type="password" class="form-control" name="confirmPassword" placeholder="Confirm Password">
+		</div>
 
-<button type="submit" class="btn btn-primary">Submit</button>
+		<button type="submit" class="btn btn-primary btn-block">Submit</button>
+	</div>
+</div>
 
 <?php form_close(); ?>
