b2704d2f3eb8e3d919da767ffbc5e6950b185a85
chloelee <chloelee@omusic.com.tw>
2019-12-26 15:38:33 +0800

    表單set_value

--- application/views/users/register.php
+++ application/views/users/register.php
@@ -7,22 +7,22 @@
 		<h1 class="text-center"><?= $title; ?></h1>
 		<div class="form-group">
 			<label>Name</label>
-			<input type="text" class="form-control" name="name" placeholder="Name">
+			<input type="text" class="form-control" name="name" value="<?php echo set_value('name'); ?>" placeholder="Name">
 		</div>
 
 		<div class="form-group">
 			<label>Zipcode</label>
-			<input type="text" class="form-control" name="zipcode" placeholder="Zipcode">
+			<input type="text" class="form-control" name="zipcode" value="<?php echo set_value('zipcode'); ?>" placeholder="Zipcode">
 		</div>
 
 		<div class="form-group">
 			<label>Email</label>
-			<input type="email" class="form-control" name="email" placeholder="Email">
+			<input type="email" class="form-control" name="email" value="<?php echo set_value('email'); ?>" placeholder="Email">
 		</div>
 
 		<div class="form-group">
 			<label>Username</label>
-			<input type="text" class="form-control" name="username" placeholder="Username">
+			<input type="text" class="form-control" name="username" value="<?php echo set_value('username'); ?>" placeholder="Username">
 		</div>
 
 		<div class="form-group">
